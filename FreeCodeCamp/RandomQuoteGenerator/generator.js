6a010b38c6eba29bef1e05c7381fe8c6d250d18e
Richard Shen <rhdsen111@yahoo.com.tw>
2016-02-28 12:17:44 -0800

    f

--- FreeCodeCamp/RandomQuoteGenerator/generator.js
+++ FreeCodeCamp/RandomQuoteGenerator/generator.js
@@ -1,50 +1,48 @@
+(function(){
+    var tweetQuote = function tweetQuote() {
+        window.open(
+            'https://twitter.com/intent/tweet?hashtags=sugardrop&related=sugardrop&text='+encodeURIComponent($('#randomQuote').text()),
+            'Share');
+    }
 
-function tweetQuote() {
-    window.open(
-        'https://twitter.com/intent/tweet?hashtags=sugardrop&related=sugardrop&text='+encodeURIComponent($('#randomQuote').text()),
-        'Share');
-}
-
-function generateQuote() {
-    var quote = $('.quote');
-    var author = $('.author');
-    var randomQuote = $('#randomQuote');
-    var authorName = $('#authorName');
-    
-    $.ajax({
-        headers: {
-            "X-Mashape-Key": "5LJ3Ye4smGmshk108kwdWJ3UtlUdp12tbvOjsnyU0gedylc9wY",
-            Accept: "application/json",
-            "Conecnt-Type": "application/x-www-form-urlencoded"
-        },
-        url: "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=movies",
-        success: function(response) {
-            var data = JSON.parse(response);
-            quote.animate({
-                opacity: 0
-            }, 500, 
-            function() {
+    var generateQuote = function generateQuote() {
+        var quote = $('.quote');
+        var author = $('.author');
+        var randomQuote = $('#randomQuote');
+        var authorName = $('#authorName');
+        
+        quote.animate({
+            opacity: 0
+        }, 250);
+        author.animate({
+            opacity: 0
+        }, 250);
+        
+        $.ajax({
+            headers: {
+                "X-Mashape-Key": "5LJ3Ye4smGmshk108kwdWJ3UtlUdp12tbvOjsnyU0gedylc9wY",
+                Accept: "application/json",
+                "Conecnt-Type": "application/x-www-form-urlencoded"
+            },
+            url: "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=movies",
+            success: function(response) {
+                var data = JSON.parse(response);
                 randomQuote.text(data["quote"]);
+                authorName.text(data["author"]);
                 quote.animate({
                     opacity: 1
-                }, 500);
-            });
-            
-            author.animate({
-                opacity: 0
-            }, 500,
-            function() {
-                authorName.text(data["author"]);
-                $(this).animate({
+                }, 250);
+                
+                author.animate({
                     opacity: 1
-                }, 500);
-            });
-        }
-    });
-}
+                }, 250);
+            }
+        });
+    }
 
-$(document).ready(function(){
-    generateQuote();
-    $('#newQuote').on("click", generateQuote);
-    $('#tweetQuote').on("click", tweetQuote);
-});
+    $(document).ready(function(){
+        generateQuote();
+        $('#newQuote').on("click", generateQuote);
+        $('#tweetQuote').on("click", tweetQuote);
+    });
+})();
