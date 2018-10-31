f37f0618d966d90709cf88920cc8dec42399959c
SugarDrop <rhdsen111@yahoo.com.tw>
2018-10-31 16:26:47 -0700

    Cityfilter

--- CityFilter/cityFilter.js
+++ CityFilter/cityFilter.js
@@ -3,6 +3,7 @@ var CityFilter = /** @class */ (function () {
         this.endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
         this.cities = [];
         this.searchBox = null;
+        this.suggestions = null;
     }
     CityFilter.prototype.findMatches = function (word) {
         return this.cities.filter(function (place, idx, arr) {
@@ -11,8 +12,17 @@ var CityFilter = /** @class */ (function () {
         });
     };
     CityFilter.prototype.displayMatches = function () {
+        var _this = this;
         var matchedPlaces = this.findMatches(this.searchBox.value);
-        console.log(matchedPlaces);
+        var generatedListItems = matchedPlaces.map(function (value, idx, arr) {
+            var regex = new RegExp(_this.searchBox.value, 'gi');
+            var city = value.city;
+            var state = value.state;
+            var cityWithHighlight = city.replace(regex, "<span class=\"highlight\">" + _this.searchBox.value + "</span>");
+            var stateWithHighlight = state.replace(regex, "<span class=\"highlight\">" + _this.searchBox.value + "</span>");
+            return "\n            <li>\n                <span class=\"name\">" + cityWithHighlight + ", " + stateWithHighlight + "</span>\n                <span class=\"population\">" + value.population + "</span>\n            </li>\n            ";
+        });
+        this.suggestions.innerHTML = generatedListItems.join('');
     };
     CityFilter.prototype.handleEvent = function (evt) {
         var _this = this;
@@ -23,10 +33,10 @@ var CityFilter = /** @class */ (function () {
         })
             .then(function (data) {
             var _a;
-            // console.log(data); 
             (_a = _this.cities).push.apply(_a, data);
             console.log(_this.cities);
         });
+        this.suggestions = document.querySelector(".suggestions");
         this.searchBox = document.querySelector(".search-input");
         this.searchBox.addEventListener("keyup", function () {
             _this.displayMatches();
@@ -35,3 +45,4 @@ var CityFilter = /** @class */ (function () {
     return CityFilter;
 }());
 document.addEventListener("DOMContentLoaded", new CityFilter());
+//# sourceMappingURL=cityFilter.js.map
--- CityFilter/cityFilter.ts
+++ CityFilter/cityFilter.ts
@@ -2,6 +2,7 @@ class CityFilter implements EventListenerObject {
     private readonly endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
     private cities = [];
     private searchBox: HTMLInputElement = null;
+    private suggestions: HTMLUListElement = null;
 
     private findMatches(word) {
         return this.cities.filter((place: any, idx: number, arr: any[]) => {
@@ -12,21 +13,34 @@ class CityFilter implements EventListenerObject {
 
     private displayMatches() {
         const matchedPlaces = this.findMatches(this.searchBox.value);
-        console.log(matchedPlaces);
+        const generatedListItems = matchedPlaces.map<string>((value: any, idx: number, arr: any[]) => {
+            const regex = new RegExp(this.searchBox.value, 'gi');
+            const city = value.city as string;
+            const state = value.state as string;
+            const cityWithHighlight = city.replace(regex, `<span class="highlight">${this.searchBox.value}</span>`);
+            const stateWithHighlight = state.replace(regex, `<span class="highlight">${this.searchBox.value}</span>`);
+            return `
+            <li>
+                <span class="name">${cityWithHighlight}, ${stateWithHighlight}</span>
+                <span class="population">${value.population}</span>
+            </li>
+            `;
+        });
+        this.suggestions.innerHTML = generatedListItems.join('');
     }
 
     handleEvent(evt: Event) {
         fetch(this.endpoint)
-            .then((blob) => {
+            .then((blob: Response) => {
                 console.log(blob);
-                return blob.json(); 
+                return blob.json();
             })
             .then((data) => {
-                // console.log(data); 
                 this.cities.push(...data);
-                console.log(this.cities); 
+                console.log(this.cities);
             });
 
+        this.suggestions = document.querySelector<HTMLUListElement>(".suggestions");
         this.searchBox = document.querySelector<HTMLInputElement>(".search-input");
         this.searchBox.addEventListener("keyup", () => {
             this.displayMatches();
